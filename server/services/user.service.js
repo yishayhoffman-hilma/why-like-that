0857a5c0f4df94b9197c3a097b6eba9cd605fcc9
yishiehoffman-hilma <yishiehoffman@hilma.tech>
2025-11-30 15:21:31 +0200

    inor changes

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,7 +1,6 @@
 const userRep = require("../repositories/user.repositories");
 async function validateLogin(username, password) {
   console.log("in service");
-
   return userRep.login(username, password).length > 0;
 }
 
