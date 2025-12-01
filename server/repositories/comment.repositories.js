f11bde34f661765cbf4c78f71b37c361335443d6
itayido <itayido@hilma.tech>
2025-12-01 14:34:06 +0200

    comment repository delete and changes the select on get comments

--- server/repositories/comment.repositories.js
+++ server/repositories/comment.repositories.js
@@ -4,7 +4,12 @@ async function getAll(postId) {
   const promiseConnection = connection.promise();
 
   const [rows] = await promiseConnection.query(
-    `SELECT * FROM comment WHERE post_id = ?`,
+    `SELECT u.username, c.id, c.user_id, c.post_id, content, deleted
+     FROM comment c
+        JOIN user u ON c.user_id = u.id
+    WHERE
+        post_id = ?
+        AND deleted = false`,
     [postId]
   );
 
@@ -15,9 +20,24 @@ async function add(user_id, post_id, content) {
   const promiseConnection = connection.promise();
 
   const [result] = await promiseConnection.query(
-    "INSERT INTO comment(user_id, content, post_id) VALUES (?, ?, ?)",
-    [user_id, post_id, content]
+    `INSERT INTO comment(user_id, content, post_id)
+     VALUES (?, ?, ?)`[(user_id, post_id, content)]
   );
 
   return result;
 }
+
+async function deleteComment(commentId) {
+  const promiseConnection = connection.promise();
+
+  const [result] = await promiseConnection.query(
+    `UPDATE comment
+    SET deleted = true
+    WHERE id = commentId`,
+    [commentId]
+  );
+
+  return result;
+}
+
+module.exports = { getAll, add, deleteComment };
