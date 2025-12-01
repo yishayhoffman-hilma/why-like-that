b3f397621017dd82abca5b0b77c76397870574f9
yishiehoffman-hilma <yishiehoffman@hilma.tech>
2025-12-01 16:38:56 +0200

    can now add and delete comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,12 +1,27 @@
 import "../App.css";
 export default function Comment(props) {
-  console.log(props);
+  const activeUser = JSON.parse(localStorage.getItem("ActiveUser"));
+  console.log(activeUser);
 
   return (
     <>
-      <p>
+      <span>
         <span className="author">{props.author}</span>: {props.content}
-      </p>
+      </span>
+
+      {activeUser.userId === props.userId ? (
+        <button
+          onClick={() => {
+            props.deleteComment(props.id);
+          }}
+        >
+          delete
+        </button>
+      ) : (
+        ""
+      )}
+      <br></br>
+      <br></br>
     </>
   );
 }
--- client/src/components/CommentsSection.jsx
+++ client/src/components/CommentsSection.jsx
@@ -3,6 +3,9 @@ import Comment from "./Comment";
 
 export default function CommentsSection(props) {
   const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState([]);
+  const activeUser = JSON.parse(localStorage.getItem("ActiveUser"));
+  const userId = activeUser.userId;
 
   useEffect(() => {
     async function fetchPosts() {
@@ -12,7 +15,6 @@ export default function CommentsSection(props) {
         );
         if (!response.ok) throw new Error("Failed to fetch posts");
         const jsonData = await response.json();
-        console.log(jsonData);
 
         setComments(jsonData.data);
       } catch (error) {
@@ -22,8 +24,66 @@ export default function CommentsSection(props) {
     fetchPosts();
   }, [props.postId]);
 
+  async function deleteComment(commentId) {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/comments/${commentId}/${userId}`,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) throw new Error("failed");
+      setComments((prev) => {
+        return prev.filter((c) => {
+          return c.id !== commentId;
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function addNewComment() {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/comments/${props.postId}/${userId}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ content: newComment }),
+        }
+      );
+      if (!response.ok) throw new Error("failed");
+      const jsonData = await response.json();
+      console.log(jsonData);
+
+      setComments((prev) => {
+        const comment = {
+          content: newComment,
+          username: activeUser.username,
+          userId,
+        };
+        return [...prev, comment];
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <input
+          onChange={(e) => {
+            setNewComment(e.target.value);
+          }}
+          value={newComment}
+        />
+
+        <button onClick={addNewComment}>add comment</button>
+      </div>
+
       {comments.length === 0
         ? "No Comments"
         : comments.map((value, index) => {
@@ -32,6 +92,9 @@ export default function CommentsSection(props) {
                 author={value.username}
                 key={index}
                 content={value.content}
+                userId={value.user_id}
+                deleteComment={deleteComment}
+                id={value.id}
               />
             );
           })}
